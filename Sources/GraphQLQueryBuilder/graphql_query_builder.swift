93b1d5d0c6baa85280cfa79ed900fb0de416e8b5
Chris McFarlen <chris@mcfarlen.us>
2024-03-05 07:54:33 -0600

    public

--- Sources/GraphQLQueryBuilder/graphql_query_builder.swift
+++ Sources/GraphQLQueryBuilder/graphql_query_builder.swift
@@ -1,46 +1,49 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-protocol Queryable {
+public protocol Queryable {
     var query: [Query] { get }
 }
 
 @resultBuilder
-struct QueryBuilder {
-    static func buildBlock() -> [Query] {
+public struct QueryBuilder {
+    public static func buildBlock() -> [Query] {
         []
     }
-    static func buildBlock(_ parts: Queryable...) -> [Query] {
+    public static func buildBlock(_ parts: Queryable...) -> [Query] {
         parts.flatMap { $0.query }
     }
 
-    static func buildIf(_ value: Queryable?) -> [Query] {
+    public static func buildIf(_ value: Queryable?) -> [Query] {
         value?.query ?? []
     }
 }
 
-protocol QueryActualParameter {
+public protocol QueryActualParameter {
     var parameterType: String? { get }
     var parameterName: String { get }
 }
 
-extension QueryActualParameter {
+public extension QueryActualParameter {
     var parameterType: String? { nil }
 }
 
 extension String: QueryActualParameter {
-    var parameterName: String { self }
+    public var parameterName: String { "\"\(self)\"" }
 }
 
+extension Int: QueryActualParameter {
+    public var parameterName: String { "\(self)" }
+}
 
-enum Query {
+public enum Query {
     case query(items: [Query])
     case field(name: String)
     case object(name: String, parameters: [String: QueryActualParameter], fields: [Query])
 }
 
 extension Query: CustomStringConvertible {
-    var description: String {
+    public var description: String {
         switch self {
             case .query(let items):
                 return "query { \(items.map(\.description).joined(separator: " ")) }"
@@ -57,27 +60,27 @@ extension Query: CustomStringConvertible {
 }
 
 extension Query: Queryable {
-    var query: [Query] {
+    public var query: [Query] {
         [self]
     }
 }
 
 extension Array: Queryable where Element == Query {
-    var query: [Query] {
+    public var query: [Query] {
         self
     }
 }
 
-func query(@QueryBuilder content: () -> [Query]) -> Query {
+public func query(@QueryBuilder content: () -> [Query]) -> Query {
     .query(items: content())
 }
 
-func object(_ name: String, withParms parameters: [String:QueryActualParameter] = [:], @QueryBuilder content: () -> [Query]) -> [Query] {
+public func object(_ name: String, withParms parameters: [String:QueryActualParameter] = [:], @QueryBuilder content: () -> [Query]) -> [Query] {
     [.object(name: name, parameters: parameters, fields: content())]
 }
 
 extension String: Queryable {
-    var query: [Query] {
+    public var query: [Query] {
         [.field(name: self)]
     }
 }
